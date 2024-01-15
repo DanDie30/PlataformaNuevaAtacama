6f221fdfdbce69e233312c15345e6cbb319c6c5b
DanDie30 <75273696+DanDie30@users.noreply.github.com>
2024-01-15 13:22:05 -0300

    Cambio en DB
    DB pool

--- src/utils/db.js
+++ src/utils/db.js
@@ -14,9 +14,15 @@ const config = {
 //configurar la conexion lista para interactuar con la base de datos
 const pool = new sql.ConnectionPool(config)
 
-//Conecta el pool con la base de datos
-pool.connect()
+app.listen(port, () => {
+    pool.connect().then(() => {
+        console.log(`Conectado a la base de datos`);
+        console.log(`Aplicacion corriendo en el puerto ${port}`);
+    }).catch(err => {
+        console.error(`Error al conectar a la base de datos: ${err.message}`);
+    });
+});
 
-//
 
 module.exports = pool
+
